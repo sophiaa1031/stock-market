7dbde51f816cc6783873a0b2d753af5b6387cc3f
sophiaa1031 <798167982@qq.com>
2020-12-27 11:12:31 +0800

    ngClass

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -9,11 +9,20 @@ import { Stock } from '../../model/stock';
 export class StockItemComponent implements OnInit {
 
   public stock: Stock;
+  public stockClasses;
 
   constructor() { }
 
   ngOnInit(): void {
     this.stock = new Stock('Test Stock Company','TSC',85,80);
+    let diff = (this.stock.price/this.stock.previousPrice) -1;
+    let largeChanges = Math.abs(diff) > 0.01;
+    this.stockClasses ={//此对象有4个key，每一个都有真/假的value,接受不了JSON对象
+      "positive": this.stock.isPositiveChange(),
+      "negative": !this.stock.isPositiveChange(),
+      "large-change": largeChanges,
+      "small-change": !largeChanges,
+    }
   }
 
   toggleFavorite(event){
