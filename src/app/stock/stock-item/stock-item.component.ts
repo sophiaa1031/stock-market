79d2daf94a7542e3e378ec010d61e9919e108307
sophiaa1031 <798167982@qq.com>
2020-12-27 11:21:41 +0800

    ngStyle

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -9,7 +9,7 @@ import { Stock } from '../../model/stock';
 export class StockItemComponent implements OnInit {
 
   public stock: Stock;
-  public stockClasses;
+  public stockStyles;
 
   constructor() { }
 
@@ -17,11 +17,9 @@ export class StockItemComponent implements OnInit {
     this.stock = new Stock('Test Stock Company','TSC',85,80);
     let diff = (this.stock.price/this.stock.previousPrice) -1;
     let largeChanges = Math.abs(diff) > 0.01;
-    this.stockClasses ={//此对象有4个key，每一个都有真/假的value,接受不了JSON对象
-      "positive": this.stock.isPositiveChange(),
-      "negative": !this.stock.isPositiveChange(),
-      "large-change": largeChanges,
-      "small-change": !largeChanges,
+    this.stockStyles ={
+      "color": this.stock.isPositiveChange()? "green":"red",
+      "font-size": largeChanges?"2em":"0.5em"
     }
   }
 
