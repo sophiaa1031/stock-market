7288c1b1e7978e65130926ccfb37fa96fe297657
sophiaa1031 <798167982@qq.com>
2021-01-06 17:32:58 +0800

    4.4.3 change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,22 @@ export class AppComponent implements OnInit {
   title = "Sophia's stock market";
 
   public stockObj:Stock;
+  private counter:number =1;
 
   ngOnInit(): void {
-    this.stockObj = new Stock('Test Stock Company','TSC',85,80);
+    this.stockObj = new Stock('Test Stock Company -'+this.counter++,'TSC',85,80);
   }
 
   onToggleFavorite(stockObj:Stock){
     console.log("Favorite for Sophia's stock",stockObj,'was triggered');
     this.stockObj.favorite = !this.stockObj.favorite;
+  }
+
+  changeStockObject(){
+    this.stockObj = new Stock('Test Stock Company -'+this.counter++,'TSC',85,80);
+  }
+
+  changeStockPrice(){
+    this.stockObj.price += 10;
   }
 }
--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { Stock } from '../../model/stock';
 
 @Component({
   selector: 'app-stock-item',
   templateUrl: './stock-item.component.html',
-  styleUrls: ['./stock-item.component.scss']
+  styleUrls: ['./stock-item.component.scss'],
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class StockItemComponent {
 
@@ -17,6 +18,10 @@ export class StockItemComponent {
 
   onToggleFavorite(event){
     this.toggleFavorite.emit(this.stock);
+  }
+
+  changeStockPrice(){
+    this.stock.price += 5;
   }
 
 }
