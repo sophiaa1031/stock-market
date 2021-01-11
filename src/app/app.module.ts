b8a06a64ac78a5bdd34c2c38e4305909e7879bf2
sophiaa1031 <798167982@qq.com>
2021-01-11 22:06:39 +0800

    6.1 diven-form: ngModel,[(ngModel)]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StockItemComponent } from './stock/stock-item/stock-item.component';
+import { CreateStockComponent } from './stock/create-stock/create-stock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StockItemComponent
+    StockItemComponent,
+    CreateStockComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
