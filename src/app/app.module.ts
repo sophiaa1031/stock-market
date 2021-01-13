6654aa744c5357a11a83d9996b12036c7703fdcc
sophiaa1031 <798167982@qq.com>
2021-01-13 22:23:31 +0800

    7.2.1 form-control: FormControl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +16,7 @@ import { CreateStockComponent } from './stock/create-stock/create-stock.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/stock/create-stock/create-stock.component.ts
+++ src/app/stock/create-stock/create-stock.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Stock } from '../../model/stock';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-create-stock',
@@ -8,22 +8,13 @@ import { Stock } from '../../model/stock';
 })
 export class CreateStockComponent {
 
-  public stock: Stock;
-  public confirmed= false;
-  public exchanges =['NYSE','NASDAQ','OTHER'];
+  public nameControl= new FormControl();
 
   constructor() {
-    this.stock = new Stock('','',0,0,'NASDAQ');
    }
 
-  createStock(stockForm){
-    console.log('Stock Form',stockForm);
-    if(stockForm.valid){
-      this.stock = stockForm.value.stock;
-      console.log('Creating stock',this.stock);
-    }else{
-      console.error('Invalid state',this.stock);
-    }
+  onSubmit(){
+    console.log('Name Control Value',this.nameControl.value);
   }
 
 }
